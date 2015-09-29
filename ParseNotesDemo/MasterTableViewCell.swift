a7d179de28c2221ead6eb99ff107b41afcb987b7
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-09-28 17:17:19 -0700

    Adding View Controllers

---
+++ ParseNotesDemo/MasterTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MasterTableViewCell.swift
+//  ParseNotesDemo
+//
+//  Created by Cynthia Whitlatch on 9/28/15.
+//  Copyright © 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+import UIKit
+
+class MasterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
